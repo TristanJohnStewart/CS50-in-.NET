b61f2a44788e2096689e10c48e12ead032ba6633
Tristan Stewart <tjstew880@gmail.com>
2025-03-05 13:17:05 +0000

    Created argument check and loop for getting user input on the number of voters.

--- CS50/src/CS50.Tideman/Original/CS50.Tideman.Original/Program.cs
+++ CS50/src/CS50.Tideman/Original/CS50.Tideman.Original/Program.cs
@@ -4,7 +4,25 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int length = args.Length;
+            if (length < 2 || length > 9)
+            {
+                Console.WriteLine("Minimum of 2 candidates up to a maximum of 9 required as commandline arguements.");
+                Environment.Exit(0);
+            }
+
+            List<string> candidateNamess = [.. args];
+
+            int numberOfVoters = 0;
+            while (true)
+            {
+                Console.WriteLine("Number of voters:");
+                numberOfVoters = Convert.ToInt16(Console.ReadLine());
+                if (numberOfVoters >= 1)
+                {
+                    break;
+                }
+            }
         }
     }
 }
